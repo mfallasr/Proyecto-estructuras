dbcfe55962855dcddc36fd24784a5b58ad4f35ef
lcruzm <lcruzm@ucenfotec.ac.cr>
2018-04-18 00:11:06 -0600

    Se agrega string de conección

---
+++ ProyectoEstructuras/ProyectoEstructuras/datos/Conector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEstructuras.datos
+{
+    public class Conector
+    {
+
+        private SqlConnection conn = null;
+        private static Conector _instance = null;
+
+        public static Conector instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Conector();
+                }
+
+                return _instance;
+            }
+        }
+
+        public Conector()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString =
+                "Data Source=localhost;" +
+                "Initial Catalog=estructuras;" +
+                "User id=SA;" +
+                "Password=4682000Leo;";
+        }
+
+
+    }
+}
